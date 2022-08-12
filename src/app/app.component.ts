44957558df134def11816b803c103241c7c46023
cheykoff <mail@michael-schoelkopf.de>
2022-08-12 12:06:16 +0200

    store url parameters in firebase

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 // import { AngularFireStore } from '@angular/fire/firestore';
 import { Firestore, collectionData, collection } from '@angular/fire/firestore';
 import { Observable } from 'rxjs';
+import { DataService } from './service/data.service';
 
 interface Exercise {
   question: string;
@@ -25,7 +26,29 @@ interface Exercise {
   styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
+  constructor(private dataService: DataService) {}
+
   title = 'quizappv1';
+  parameters = {};
+  ngOnInit(): void {
+    console.log('app.component - ngOnInit');
+    console.log(window.location.href);
+    this.getParameters();
+  }
+
+  getParameters() {
+    let urlString = window.location.href;
+    console.log('urlString: ' + urlString);
+    let paramString = urlString.split('?')[1];
+    console.log('paramString: ' + paramString);
+    if (paramString) {
+      this.parameters = paramString.split('&');
+      console.log('params: ' + this.parameters);
+      console.log(typeof this.parameters);
+    }
+    this.dataService.storeUrlParameters(this.parameters);
+  }
+
   /*
   exercise$: Observable<Exercise[]>;
 
@@ -34,5 +57,4 @@ export class AppComponent {
     this.exercise$ = <Observable<Exercise[]>>collectionData(exercises);
   }
   */
-  
 }
--- src/app/service/data.service.ts
+++ src/app/service/data.service.ts
@@ -26,9 +26,31 @@ import {
 export class DataService {
   constructor(private http: HttpClient, private shared: SharedService) {}
 
+  urlParameters = {};
+
+  storeUrlParameters(parameters) {
+    console.log('storeUrlParameters (data.service.ts)');
+    console.log('parameters (data.service.ts): ' + parameters);
+    this.urlParameters = parameters;
+    this.http
+      .post(
+        'https://quizapp-d018b-default-rtdb.europe-west1.firebasedatabase.app/parameters.json',
+        //{ points, schoolClass, createdAt: serverTimestamp() }
+        {
+          parameters,
+          createdAt: Date.now(),
+          url: window.location.href,
+        }
+      )
+      .subscribe((response) => {
+        console.log(response);
+      });
+  }
+
   storeResult(points) {
     console.log('storeResult (data.service.ts)');
     const schoolClass = this.shared.getSchoolClass();
+    const parameters = this.urlParameters;
 
     this.http
       .post(
@@ -39,6 +61,7 @@ export class DataService {
           schoolClass,
           createdAt: Date.now(),
           url: window.location.href,
+          parameters,
         }
       )
       .subscribe((response) => {
