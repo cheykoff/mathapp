b385a8d0fa53567c1d842206d638d1c0853371a3
cheykoff <mail@michael-schoelkopf.de>
2022-08-19 00:07:10 +0200

    fix writing in db

--- src/app/service/data.service.ts
+++ src/app/service/data.service.ts
@@ -1,22 +1,6 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { SharedService } from '../shared/shared.service';
-import { map } from 'rxjs/operators';
-import {
-  provideFirestore,
-  getFirestore,
-  serverTimestamp,
-  FieldValue,
-} from '@angular/fire/firestore';
-import {
-  Database,
-  set,
-  ref,
-  update,
-  onValue,
-  remove,
-} from '@angular/fire/database';
-import { getDatabase } from 'firebase/database';
 
 @Injectable({
   providedIn: 'root',
@@ -29,30 +13,37 @@ export class DataService {
   storeUrlParameters(parameters) {
     console.log('storeUrlParameters', parameters);
     this.urlParameters = parameters;
-    this.http.post(
-      'https://quizapp-d018b-default-rtdb.europe-west1.firebasedatabase.app/test.json',
-      //'https://quizapp-d018b-default-rtdb.europe-west1.firebasedatabase.app/parameters.json',
-      {
-        parameters,
-        startTime: Date.now(),
-        url: window.location.href,
-      }
-    );
+    this.http
+      .post(
+        'https://quizapp-d018b-default-rtdb.europe-west1.firebasedatabase.app/parameters.json',
+        {
+          parameters,
+          createdAt: Date.now(),
+          url: window.location.href,
+        }
+      )
+      .subscribe((response) => {
+        console.log(response);
+      });
   }
 
   storeResult(points) {
     const schoolClass = this.shared.getSchoolClass();
     const parameters = this.urlParameters;
 
-    this.http.post(
-      'https://quizapp-d018b-default-rtdb.europe-west1.firebasedatabase.app/result.json',
-      {
-        points,
-        schoolClass,
-        createdAt: Date.now(),
-        url: window.location.href,
-        parameters,
-      }
-    );
+    this.http
+      .post(
+        'https://quizapp-d018b-default-rtdb.europe-west1.firebasedatabase.app/result.json',
+        {
+          points,
+          schoolClass,
+          startTime: Date.now(),
+          url: window.location.href,
+          parameters,
+        }
+      )
+      .subscribe((response) => {
+        console.log(response);
+      });
   }
 }
