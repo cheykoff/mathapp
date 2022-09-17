f7c4a27f301b296ec20909c681e4cbd1261b6896
cheykoff <mail@michael-schoelkopf.de>
2022-09-17 19:54:42 +0200

    remove writing in realtime db, add quizId

--- src/app/service/data.service.ts
+++ src/app/service/data.service.ts
@@ -25,47 +25,21 @@ export class DataService {
       startTime: Date.now(),
       url: window.location.href,
     });
-    /*
-    if (parameters.length === 1) {
-      const userId = parameters[0].split('=')[1];
-      this.http
-        .post(
-          'https://quizapp-d018b-default-rtdb.europe-west1.firebasedatabase.app/parameters.json',
-          {
-            userId: userId,
-            startTime: Date.now(),
-            url: window.location.href,
-          }
-        )
-        .subscribe((response) => {});
-    } else {
-      this.urlParameters = parameters;
-      this.http
-        .post(
-          'https://quizapp-d018b-default-rtdb.europe-west1.firebasedatabase.app/parameters.json',
-          {
-            parameters,
-            startTime: Date.now(),
-            url: window.location.href,
-          }
-        )
-        .subscribe((response) => {});
-    }
-    */
   }
 
   docId = '';
+  quizId = '';
   sessionId = '';
 
   storeSessionId(sessionId) {
     this.sessionId = sessionId;
-    this._store.collection(`session2`).add({
+    this._store.collection(`sessions`).add({
       sessionId: sessionId,
       url: window.location.href,
       startTime: serverTimestamp(),
     });
     this._store
-      .collection('session2', (ref) => ref.where('sessionId', '==', sessionId))
+      .collection('sessions', (ref) => ref.where('sessionId', '==', sessionId))
       .get()
       .subscribe((snaps) => {
         snaps.forEach((snap) => {
@@ -77,42 +51,46 @@ export class DataService {
   }
 
   storeMode() {
-    this._store.doc(`/session2/${this.docId}`).update({
+    this._store.doc(`/sessions/${this.docId}`).update({
       mode: 'quiz',
     });
     console.log('docId: ' + this.docId);
   }
 
   storeSchoolClass(className: number) {
+    /*
     this._store.doc(`/session2/${this.docId}`).update({
       schoolClass: className,
     });
+    */
     this._store.collection(`quizzes`).add({
       schoolClass: className,
       sessionId: this.sessionId,
       url: window.location.href,
       startTime: serverTimestamp(),
     });
+    this._store
+      .collection('quizzes', (ref) =>
+        ref.where('sessionId', '==', this.sessionId)
+      )
+      .get()
+      .subscribe((snaps) => {
+        snaps.forEach((snap) => {
+          this.quizId = snap.id;
+          console.log('sessionId: ' + this.sessionId);
+          console.log('snap.id' + snap.id);
+        });
+      });
   }
 
   storeResult(points) {
-    const schoolClass = this.shared.getSchoolClass();
-    const parameters = this.urlParameters;
     /* 
-    this.http
-      .post(
-        'https://quizapp-d018b-default-rtdb.europe-west1.firebasedatabase.app/result.json',
-        {
-          points,
-          schoolClass,
-          endTime: Date.now(),
-          url: window.location.href,
-          parameters,
-        }
-      )
-      .subscribe((response) => {});
-        */
     this._store.doc(`/session2/${this.docId}`).update({
+      points: points,
+      endTime: serverTimestamp(),
+    });
+    */
+    this._store.doc(`/quizzes/${this.quizId}`).update({
       points: points,
       endTime: serverTimestamp(),
     });
