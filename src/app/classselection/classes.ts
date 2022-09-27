7c9a0bfedf4bd8380d74f54313e80087fbe5700c
cheykoff <mail@michael-schoelkopf.de>
2022-09-27 20:32:48 +0200

    chore(classlist): refactored code

---
+++ src/app/classselection/classes.ts
@@ -0,0 +1,15 @@
+export interface SchoolClass {
+  level: number;
+  disabled?: boolean;
+}
+
+export const schoolClasses: SchoolClass[] = [
+  { level: 5 },
+  { level: 6 },
+  { level: 7 },
+  { level: 8, disabled: true },
+  { level: 9, disabled: true },
+  { level: 10, disabled: true },
+  { level: 11, disabled: true },
+  { level: 12, disabled: true },
+];
