f4c1de1e72d3d11714430f2b6cb6c4d099885fa3
cheykoff <mail@michael-schoelkopf.de>
2022-10-17 23:13:06 +0200

    fix: countdown

--- src/app/countdown.pipe.ts
+++ src/app/countdown.pipe.ts
@@ -5,18 +5,10 @@ import { Pipe, PipeTransform } from '@angular/core';
 })
 export class CountDownPipe implements PipeTransform {
   transform(value: number): string {
-    const minutes: number = Math.floor(value / 60);
+    const minutes: number = Math.ceil(value / 60);
     if (minutes < 1) {
-      return Math.floor(value - minutes * 60) + ' s';
+      return Math.floor(value - (minutes - 1) * 60) + ' s';
     }
     return minutes + ' min';
-    // return ('00' + minutes).slice(-2) + ' min';
-    /* with seconds
-    return (
-      ('00' + minutes).slice(-2) +
-      ':' +
-      ('00' + Math.floor(value - minutes * 60)).slice(-2)
-    );
-    */
   }
 }
