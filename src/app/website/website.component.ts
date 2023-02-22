e78ca5ea7fc365a04fee8e81651b6519ed35eb99
cheykoff <mail@michael-schoelkopf.de>
2023-02-22 12:59:30 +0100

    fix: scss import

--- src/app/website/website.component.ts
+++ src/app/website/website.component.ts
@@ -3,13 +3,10 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-website',
   templateUrl: './website.component.html',
-  styleUrls: ['./website.component.css']
+  styleUrls: ['./website.component.scss'],
 })
 export class WebsiteComponent implements OnInit {
+  constructor() {}
 
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
