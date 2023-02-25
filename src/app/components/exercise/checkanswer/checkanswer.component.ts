c33d9e56360b3ca1b4c36e1f15ca087caa680571
cheykoff <mail@michael-schoelkopf.de>
2023-02-25 09:32:14 +0100

    chore: delete checkanswer component (I need only a service)

--- src/app/components/exercise/checkanswer/checkanswer.component.ts
+++
@@ -1,8 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-  selector: 'app-checkanswer',
-  templateUrl: './checkanswer.component.html',
-  styleUrls: ['./checkanswer.component.css'],
-})
-export class CheckanswerComponent {}
