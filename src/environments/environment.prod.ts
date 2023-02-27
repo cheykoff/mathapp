8cc4af4ad70ee0fb261b24649f15773f30b48e97
cheykoff <mail@michael-schoelkopf.de>
2023-02-27 15:25:51 +0100

    firebase: use prod db

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,12 +1,12 @@
 export const environment = {
   firebase: {
-    projectId: 'quizapp-d018b',
-    appId: '1:628501369285:web:74eb20b73f6e1891fffbac',
-    storageBucket: 'quizapp-d018b.appspot.com',
+    projectId: 'matheapp-prod',
+    appId: '1:82563327798:web:c1d55c6a992a73b5a27597',
+    storageBucket: 'matheapp-prod.appspot.com',
     locationId: 'europe-west3',
-    apiKey: 'AIzaSyC9VBHxz2xe5x2V5LJB87VpQVrtwSWioEI',
-    authDomain: 'quizapp-d018b.firebaseapp.com',
-    messagingSenderId: '628501369285',
+    authDomain: 'matheapp-prod.firebaseapp.com',
+    apiKey: 'AIzaSyCOY6gH3ue3JI0RK75tmgjqfClpBATw1TY',
+    messagingSenderId: '82563327798',
   },
   production: true,
   useEmulators: false,
